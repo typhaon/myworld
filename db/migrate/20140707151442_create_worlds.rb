dfe1d4955bf94aafe69d6b44d8fcb536f1879bd8
Typhaon <davidgodwin788@gmail.com>
2014-07-17 10:42:55 -0400

    Fix migration syntax.

--- db/migrate/20140707151442_create_worlds.rb
+++ db/migrate/20140707151442_create_worlds.rb
@@ -4,7 +4,7 @@ class CreateWorlds < ActiveRecord::Migration
       t.string :name, null: false
       t.integer :max_rows, null: false
       t.integer :max_columns, null: false
-      t.string :cells, :array => true, default: {'0'}
+      t.string :cells, array: true, default: '{0}'
 
       t.timestamps
     end
